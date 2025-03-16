8c07cc7976d1dd7ecf5f92089d975830c7eb2efc
Gábor Csárdi <csardi.gabor@gmail.com>
2025-03-16 19:11:47 +0100

    Shiny kmeans example: add tracing to the third reactive

--- inst/examples/kmeans2/global.R
+++
@@ -1,7 +0,0 @@
-Sys.setenv(OTEL_SERVICE_NAME = "kmeans-shiny-app")
-tracer <- opentelemetry::setup_default_tracer("kmeans-shiny-app")
-app_span <- tracer$start_span("app", parent = NULL, scope = NULL)
-onStop(function() {
-  app_span$end()
-  rm(tracer, app_span, envir = globalenv())
-})
