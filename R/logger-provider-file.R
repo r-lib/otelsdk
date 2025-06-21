12d6524dc442972047e0f3202e63c23d0b6eb5c4
Gábor Csárdi <csardi.gabor@gmail.com>
2025-06-21 07:45:27 +0200

    Fix env vars for JSON file logger provider

--- R/logger-provider-file.R
+++ R/logger-provider-file.R
@@ -7,23 +7,23 @@ logger_provider_file_new <- function(
   rotate_size = NULL
 ) {
   file_pattern <- as_string(file_pattern) %||%
-    get_env(file_exporter_traces_file_envvar) %||%
+    get_env(file_exporter_logs_file_envvar) %||%
     get_env(file_exporter_file_envvar)
   alias_pattern <- as_string(alias_pattern) %||%
-    get_env(file_exporter_traces_alias_envvar) %||%
+    get_env(file_exporter_logs_alias_envvar) %||%
     get_env(file_exporter_alias_envvar) %||%
     empty_atomic_as_null(sub("%N", "latest", file_pattern))
   flush_interval <- as_difftime_spec(flush_interval) %||%
-    as_difftime_env(file_exporter_traces_flush_interval_envvar) %||%
+    as_difftime_env(file_exporter_logs_flush_interval_envvar) %||%
     as_difftime_env(file_exporter_flush_interval_envvar)
   flush_count <- as_count(flush_count, null = TRUE) %||%
-    as_count_env(file_exporter_traces_flush_count_envvar, positive = TRUE) %||%
+    as_count_env(file_exporter_logs_flush_count_envvar, positive = TRUE) %||%
     as_count_env(file_exporter_flush_count_envvar, positive = TRUE)
   file_size <- as_bytes(file_size) %||%
-    as_bytes_env(file_exporter_traces_file_size_envvar) %||%
+    as_bytes_env(file_exporter_logs_file_size_envvar) %||%
     as_bytes_env(file_exporter_file_size_envvar)
   rotate_size <- as_bytes(rotate_size) %||%
-    as_count_env(file_exporter_traces_rotate_size_envvar) %||%
+    as_count_env(file_exporter_logs_rotate_size_envvar) %||%
     as_count_env(file_exporter_rotate_size_envvar)
 
   self <- new_object(
