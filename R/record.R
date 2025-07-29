c7ed505d04740657cd88d72a642d6a093db6dfde
Gábor Csárdi <csardi.gabor@gmail.com>
2025-07-29 12:14:11 +0200

    with_otel_record() records metrics as well

--- R/record.R
+++ R/record.R
@@ -8,23 +8,31 @@ otel_restore_cache <- function(copy) {
 
 #' Record OpenTelemetry output, for testing purposes
 #'
-#' You can use this function to test the OpenTelemetry output is
+#' You can use this function to test that OpenTelemetry output is
 #' correctly generated for your package or application.
 #'
 #' It evaluates the supplied expression, collects OpenTelemetry output
 #' from it and returns it.
 #'
+#' Note: `with_otel_record()` cannot record logs yet.
+#'
 #' @param expr Expression to evaluate.
-#' @param provider_args A list of arguments to pass to the in-memory
-#'   OpenTelemetry trace provider.
 #' @param what Character vector, type(s) of OpenTelemetry output to collect.
-#'   Currently only `"traces"` is supported.
-#' @return A list with the output for each output type. Currently only
-#'   contains `traces`.
+#' @param tracer_opts Named list of options to pass to the tracer provider.
+#' @param meter_opts Named list of options to pass to the meter provider.
+#' @return A list with the output for each output type. Entries:
+#'   * `value`: value of `expr`.
+#'   * `traces`: the recorded spans, if requested in `what`.
+#'   * `metrics`: the recorded metrics measurements, if requests in `what`.
 #'
 #' @export
 
-with_otel_record <- function(expr, provider_args = list(), what = c("traces")) {
+with_otel_record <- function(
+  expr,
+  what = c("traces", "metrics"),
+  tracer_opts = list(),
+  meter_opts = list()
+) {
   # save current otel cache, restore on exit
   copy <- otel_save_cache()
   on.exit(otel_restore_cache(copy), add = TRUE)
@@ -32,10 +40,11 @@ with_otel_record <- function(expr, provider_args = list(), what = c("traces")) {
   # create new providers
   tmp <- new.env(parent = emptyenv())
   if ("traces" %in% what) {
-    tmp[["tracer_provider"]] <- do.call(
-      tracer_provider_memory_new,
-      provider_args
-    )
+    tmp[["tracer_provider"]] <- tracer_provider_memory_new(tracer_opts)
+  }
+  if ("metrics" %in% what) {
+    tmp[["meter_provider"]] <- meter_provider_memory_new(meter_opts)
+    on.exit(tmp[["meter_provider"]]$shutdown(), add = TRUE)
   }
   otel_restore_cache(tmp)
 
@@ -45,5 +54,9 @@ with_otel_record <- function(expr, provider_args = list(), what = c("traces")) {
   }
 
   # return recorded results
-  list(value = value, traces = tmp[["tracer_provider"]]$get_spans())
+  list(
+    value = value,
+    traces = tmp[["tracer_provider"]]$get_spans(),
+    metrics = tmp[["meter_provider"]]$get_metrics()
+  )
 }
