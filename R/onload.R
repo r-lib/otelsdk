01d8cd6ee06c2a4415ed9abb9bbe3e428851f0a1
Gábor Csárdi <csardi.gabor@gmail.com>
2025-09-11 10:52:09 +0200

    Adjust test coverage config, exclusions

--- R/onload.R
+++ R/onload.R
@@ -1,10 +1,8 @@
 the <- new.env(parent = emptyenv())
 
-# nocov start
 .onLoad <- function(libname, pkgname) {
   the$span_kinds <- span_kinds
   the$span_status_codes <- span_status_codes
   the$default_resource_attributes <- default_resource_attributes()
   ccall(otel_init_constants, the)
 }
-# nocov end
