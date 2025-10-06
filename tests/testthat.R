4822599888ad4caca24bdb6de10eab75bdd508b9
Gábor Csárdi <csardi.gabor@gmail.com>
2025-10-06 09:12:11 +0200

    Skip tests on CRAN
    Some tests are a bit flaky, unfortunately.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,21 +1,15 @@
-# This file is part of the standard setup for testthat.
-# It is recommended that you do not modify it.
-#
-# Where should you do additional test configuration?
-# Learn more about the roles of various files in:
-# * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
-# * https://testthat.r-lib.org/articles/special-files.html
-
 library(testthat)
 library(otelsdk)
 
-if (requireNamespace("testthatlabs", quietly = TRUE)) {
-  test_check(
-    "otelsdk",
-    reporter = asNamespace("testthatlabs")$non_interactive_reporter$new(
-      "otelsdk"
+if (Sys.getenv("NOT_CRAN") != "") {
+  if (requireNamespace("testthatlabs", quietly = TRUE)) {
+    test_check(
+      "otelsdk",
+      reporter = asNamespace("testthatlabs")$non_interactive_reporter$new(
+        "otelsdk"
+      )
     )
-  )
-} else {
-  test_check("otelsdk")
+  } else {
+    test_check("otelsdk")
+  }
 }
