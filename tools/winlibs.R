70afbad55f14986609cabdc61f042e3d23211dc1
Gábor Csárdi <csardi.gabor@gmail.com>
2026-03-10 23:13:42 +0100

    Add support for R 4.2.0 on Windows
    R 4.1.x and R 4.0.x do not work currently, because
    opentelemetry-cpp does not compile with the Rtools40
    gcc 8.3.0 compiler.

---
+++ tools/winlibs.R
@@ -0,0 +1,29 @@
+if (!file.exists("windows/protobuf/protobuf/include/google/protobuf/descriptor.h")){
+  unlink("windows/protobuf", recursive = TRUE, )
+  url <- "https://github.com/rwinlib/protobuf/archive/v21.12.tar.gz"
+  download.file(url, basename(url), quiet = TRUE)
+  dir.create("windows/protobuf", showWarnings = FALSE, recursive = TRUE)
+  untar(basename(url), exdir = "windows/protobuf", tar = 'internal')
+  unlink(basename(url))
+  setwd("windows/protobuf")
+  file.rename(list.files(), 'protobuf')
+  file.rename("protobuf/lib", "protobuf/lib-10.4.0")
+  invisible()
+}
+
+# libcurl is not needed on R 4.2.0, because Rtools42 has it
+if (
+  getRversion() < "4.2.0" &&
+    !file.exists("windows/libcurl/libcurl/include/curl/curl.h")
+) {
+  unlink("windows/libcurl", recursive = TRUE)
+  url <- "https://github.com/rwinlib/libcurl/archive/v7.84.0.tar.gz"
+  download.file(url, basename(url), quiet = TRUE)
+  dir.create("windows/libcurl", showWarnings = FALSE, recursive = TRUE)
+  untar(basename(url), exdir = "windows/libcurl", tar = 'internal')
+  unlink(basename(url))
+  setwd("windows/libcurl")
+  file.rename(list.files(), 'libcurl')
+  file.rename("libcurl/lib", "libcurl/lib-10.4.0")
+  invisible()
+}
